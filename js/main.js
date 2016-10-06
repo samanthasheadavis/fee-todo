83233ff87fa5fa2984685dc27b687263aa1e74b2
samanthasheadavis <samantha.shea.davis@gmail.com>
2016-10-05 23:02:01 -0400

    basic functionality in place

--- js/main.js
+++ js/main.js
@@ -11,6 +11,8 @@ $(document).ready(function() {
                 var todoString = $('.new-todo').val();
                 $('.new-todo').val('');
                 todo.createTodo(todoString);
+                todo.addRemain();
+
 
             });
         },
@@ -21,31 +23,68 @@ $(document).ready(function() {
             var btn = $('<button>').attr('class', 'check').prependTo(todoItem);
             var text = $('<p>').attr('class', 'text').html(todoString).appendTo(todoItem);
             var deleteTodo = $('<button>').attr('class', 'delete').html('X').appendTo(todoItem);
-            todo.handleRemain();
             todo.editTodo(todoString);
 
         },
         // increment "items left" up or down
         handleRemain: function() {
             $('.incomplete-items').html(todo.remainingTodo);
-            todo.remainingTodo++;
         },
+
+        addRemain: function() {
+          todo.remainingTodo++;
+          $('.incomplete-items').html(todo.remainingTodo);
+
+        },
+
+        subtractRemain: function() {
+          todo.remainingTodo--;
+          $('.incomplete-items').html(todo.remainingTodo);
+
+        },
+
 
         editTodo: function(todoString) {
             //trigger function if todo clicked on, dynamically create editableTodo and populate with todoString.
             $('.text').click(function(event) {
-                console.log("The paragraph was clicked.");
-                var editableTodo = $("<input>").val(todoString);
-            $(this).replaceWith(editableTodo); //"this" refers to .text class item clicked on.
-            //save new todo text and when input button submitted, replace with old <p> text class containing updated todo
-            $('input').submit(function(event) {
-              todoString = $('input').val();
-              console.log(todoString);
+                var editableTodo = $("<input>").attr('class', 'newInput').val(todoString);
+                $(this).replaceWith(editableTodo); //"this" refers to .text class item clicked on.
+
             });
+        },
+        // use event delegation to toggle delete button on hover and assign click event to delete button
+        deleteTodo: function() {
+            $('main').on('mouseenter mouseleave', 'article', function(event) {
+              $(this).children('.delete').toggle();
+
             });
+            $('main').on('click', '.delete', function() {
+                $(this).parents('article').remove();
+                todo.subtractRemain();
+            });
+
+        },
+
+        //checkOff adds the 'checked' class to the article whose check button is being clicked
+        checkOff: function() {
+          $('main').on('click', '.check', function() {
+            $(this).addClass('btnCheck');
+            $(this).parents('article').addClass('checked');
+            todo.subtractRemain();
+          });
         }
     };
 
     todo.getTodo();
     todo.handleRemain(); // need to trigger in order to count 0 items in the beginning.
+    todo.deleteTodo();
+    todo.checkOff();
 });
+
+// left to do:
+// get clear completed button to delete all completed items
+// fix bug where completed items that are deleted run deleteTodo twice
+// store todo items in an array. could items left just be array.length?
+
+// all the different views stuff
+// finicky css stuff
